8294caf54b0458842793753556bad6c16d2ec187
Matt Denny <matthewjdenny@gmail.com>
2016-05-19 16:47:30 -0400

    start plotting script

---
+++ Scripts/Publication_Quality_Plots.R
@@ -0,0 +1,17 @@
+# publication quality graphics
+# Matthew J. Denny
+# email mdenny@psu.edu with questions or comments
+
+# In this script, I will provide several examples and seek to introduce you to
+# the kinds of things one should always do to produce really clean plots for
+# publication. I cannot possibly hope to cover every siutation, but I always
+# find myself doing the same sorts of things whenever I make plots for
+# publication. This is really where R shines. We will be using the base R
+# graphics and ggplot2.
+
+# install ggplot2
+# install.packages("ggplot2", dependencies = TRUE)
+
+# preliminaries
+rm(list = ls())
+setwd("~/Desktop")
